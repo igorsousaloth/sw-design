e0cfc2c139ec3a1564c0b805c965129dc1dd138a
1600447 <1600447@sala31204.fit.net>
2017-03-13 09:20:01 -0300

    Persistência dos dados em arquivo

--- src/br/edu/impacta/ads/Contato.java
+++ src/br/edu/impacta/ads/Contato.java
@@ -3,34 +3,30 @@ package br.edu.impacta.ads;
 public class Contato {
 	private String nome;
 	private String telefone;
-	
+	public Contato() {
+		super();
+	}
 	public Contato(String nome, String telefone) {
 		super();
 		this.nome = nome;
 		this.telefone = telefone;
 	}
-
 	public String getNome() {
 		return nome;
 	}
-
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
-
 	public String getTelefone() {
 		return telefone;
 	}
-
 	public void setTelefone(String telefone) {
 		this.telefone = telefone;
 	}
-
 	@Override
 	public String toString() {
 		return "Contato [nome=" + nome + ", telefone=" + telefone + "]";
 	}
-
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -39,7 +35,6 @@ public class Contato {
 		result = prime * result + ((telefone == null) ? 0 : telefone.hashCode());
 		return result;
 	}
-
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -61,7 +56,6 @@ public class Contato {
 			return false;
 		return true;
 	}
-
 	
 }
 
